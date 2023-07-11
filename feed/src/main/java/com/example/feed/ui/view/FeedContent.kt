73d30f2f29fd4588c27127c89a97c9a7fff936fe
HossamSadekk <66652255+HossamSadekk@users.noreply.github.com>
2023-07-11 04:43:17 -0700

    Adding functionality of scroll to up button to facilitate to user to scroll down so much to scroll up again faster

--- feed/src/main/java/com/example/feed/ui/view/FeedContent.kt
+++ feed/src/main/java/com/example/feed/ui/view/FeedContent.kt
@@ -1,7 +1,10 @@
 package com.example.feed.ui.view
 
 import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -12,11 +15,15 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.common.widget.GoUp
 import com.example.common.widget.ShimmerItem
 import com.example.feed.ui.FeedEvent
 import com.example.feed.ui.FeedViewModel
@@ -27,14 +34,21 @@ import com.example.feed.ui.view.everythingComponent.Categories
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.count
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 @Composable
 fun FeedContent(viewState: FeedViewState, feedViewModel: FeedViewModel) {
-
+    val scrollState = rememberLazyListState()
+    val scope = rememberCoroutineScope()
+    val showButton = remember {
+        derivedStateOf {
+            scrollState.firstVisibleItemIndex > 0
+        }
+    }
     Box(Modifier.fillMaxSize()) {
         val articles = viewState.articles.collectAsLazyPagingItems()
 
-        val scrollState = rememberLazyListState()
+
 
         LazyColumn(state = scrollState, contentPadding = PaddingValues(bottom = 90.dp)) {
             item {
@@ -45,12 +59,12 @@ fun FeedContent(viewState: FeedViewState, feedViewModel: FeedViewModel) {
                         .fillMaxWidth(), feedViewModel
                 )
             }
-
+            // displaying the sources section.
             item {
                 CategoryTitle("Sources", topPadding = 20.dp, bottomPadding = 20.dp)
                 SourcesItems(viewState)
             }
-
+            // the everything section ( categories )
             item {
                 CategoryTitle("Everything", topPadding = 20.dp, bottomPadding = 20.dp)
                 Categories(categories = feedViewModel.categoriesState.value,
@@ -80,6 +94,13 @@ fun FeedContent(viewState: FeedViewState, feedViewModel: FeedViewModel) {
             }
 
 
+        }
+    }
+    AnimatedVisibility(visible = showButton.value, enter = fadeIn(), exit = fadeOut()) {
+        GoUp {
+            scope.launch {
+                scrollState.scrollToItem(0)
+            }
         }
     }
 }
